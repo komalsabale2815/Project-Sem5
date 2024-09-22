03afe570aa4d4d1ca48323af210d6ff98be4c1e4
Komal <146917919+komalsabale2815@users.noreply.github.com>
2024-09-22 19:27:13 +0530

    Add files via upload

---
+++ src/data/clothesCollections.jsx
@@ -0,0 +1,48 @@
+// src/data/clothesCollection.js
+import img1 from "../assets/c1.png";
+import img2 from "../assets/c2.png";
+import img3 from "../assets/c3.png";
+import img4 from "../assets/c4.png";
+import img5 from "../assets/c5.png";
+import img6 from "../assets/c6.png";
+
+
+export const clothesCollection = [
+  {
+    id: 1,
+    title: "Stylish T-Shirt",
+    price: 25,
+    img: img1,
+  },
+  {
+    id: 2,
+    title: "Classic Jeans",
+    price: 40,
+    img: img2,
+  },
+  {
+    id: 3,
+    title: "Elegant Dress",
+    price: 60,
+    img: img3,
+  },
+  {
+    id: 4,
+    title: "Elegant Dress",
+    price: 60,
+    img: img3,
+  },
+  {
+    id: 5,
+    title: "Elegant Dress",
+    price: 60,
+    img: img3,
+  },
+  {
+    id: 6,
+    title: "Elegant Dress",
+    price: 60,
+    img: img3,
+  },
+  // Add more clothing items here
+];
