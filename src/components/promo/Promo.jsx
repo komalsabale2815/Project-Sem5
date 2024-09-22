67ed89e3661dbac719d65a3d722a66b9199762ba
Komal <146917919+komalsabale2815@users.noreply.github.com>
2024-09-22 19:32:19 +0530

    Add files via upload

---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Promo.css';
+
+const Promo = () => {
+
+    return(
+        <>
+        <div className='promo-text-section'>
+            <div className='promo-text'>
+                <p>Thousands of new items daily. All up to 90% off.</p>
+                <p>Over 55k brands (yes, really), all up to 90% off estimated retail. Score styles you love the sustainable way.</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Promo;
